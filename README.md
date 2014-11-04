2eda7380d3e639c2e3af1fc222ff4367dcb415a2
qianlifeng <qianlf2008@163.com>
2014-11-04 17:17:01 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+Inspired by [wox.plugin.putty](https://github.com/kosz78/wox.plugin.putty), this plugin let you launch [kitty](http://www.9bis.net/kitty/) sessions from Wox.
+
+![](http://ww1.sinaimg.cn/large/5d7c1fa4jw1elz31mxr41j20m804uaa4.jpg)  
+
+#How to use
+1. Install this plugin via `wpm install kitty`
+2. Edit config.json file in this plugin folder and set the the kitty folder path in your disk
+
+#Notice
+**This plugin doesn't support sessions in folders.**
---
+++ main.py
@@ -0,0 +1,32 @@
+#encoding=utf8
+import json
+import os,sys,webbrowser
+import subprocess
+from wox import Wox
+from os import listdir
+from os.path import isfile, join
+
+class Kitty(Wox):
+
+    def load_session(self,kitty_path):
+        session_path = os.path.join(kitty_path,"Sessions")
+        files = [f for f in listdir(session_path) if isfile(join(session_path,f))]
+        return files
+
+    def query(self,query):
+        with open(os.path.join(os.path.dirname(__file__),"config.json"), "r") as content_file:
+            config = json.loads(content_file.read())
+        kitty_folder_path = config["kittyPath"]
+        kitty_path = os.path.join(kitty_folder_path,"kitty.exe")
+        sessions = self.load_session(kitty_folder_path)
+        res = []
+        for p in sessions:
+            if query in p:
+                res.append({"Title": p,"IcoPath":"kitty.png","JsonRPCAction":{"method": "open_session", "parameters": [kitty_path.replace("\\","\\\\"),p]}})
+        return res
+
+    def open_session(self,kitty_path,session_name):
+        subprocess.call('{} -load "{}"'.format(kitty_path,session_name))
+
+if __name__ == "__main__":
+    Kitty()
